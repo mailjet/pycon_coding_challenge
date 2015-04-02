634d658d6cfc58830e7e72bcb75b7430036d9084
Tyler Nappy <tylernappy@gmail.com>
2015-04-02 17:52:38 -0400

    updated views and controller for flow

--- index.rb
+++ index.rb
@@ -22,23 +22,41 @@ class Coder < ActiveRecord::Base
   validates_presence_of :email
 end
 
+#landing page
 get "/" do
+  debugger
+  debugger
   erb :start
 end
 
-post "/coding_challenge" do
+#create landing page (just backend)
+post "/coding_challenge/create" do
   array = determine_coding_challenge_variant
-  Coder.create!(name: params[:name], email: params[:email], company: params[:company], position: params[:position], array: array)
+  @coder = Coder.create(name: params[:name], email: params[:email], company: params[:company], position: params[:position], array: array)
+  redirect to("/coding_challenge/#{@coder.id}")
+end
+
+#actual coding session
+get "/coding_challenge/:id" do
+  @coder = Coder.find(params[:id])
   erb :coding_challenge
 end
 
+#save the results
 post "/save/:id" do
+  # debugger
+  # debugger
   @coder = Coder.find(params[:id])
-  @coder.update_attributes(answer: params[:answer], correct: params[:correct], time: params[:time])
+  @coder.update_attributes(answer: params[:answer], is_correct: is_correct(@coder.array), time: params[:time])
   redirect to("/")
 end
 
 def determine_coding_challenge_variant
   array = 100.times.map{Random.rand(100)}
-  return coding_challenge_variant
+  return array
+end
+
+#method will change depending on challenge
+def is_correct(array)
+
 end
